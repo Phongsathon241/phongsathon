9b233943378236fda32b18ad1b8128590eeafa45
Phongsathon241 <phongsathon.tha@rmutto.ac.th>
2024-11-30 12:20:56 +0700

    profile

---
+++ app/routes/profile.tsx
@@ -0,0 +1,33 @@
+function MyProfile () {
+    return (
+        <div className="border-4 border-color: rgb(67 56 202) text-center">
+            <h1>
+             <strong>
+                work experience
+             </strong>
+            </h1>
+
+            <p>
+                Name : Phongsathon Thnam<br/>
+                ID : 0254545-3    
+            </p>
+            <MyEducation/>
+        </div>
+    );
+}
+
+function MyEducation () {
+    return (
+        <div>
+            <ul>
+                <li>Information Thecnology</li>
+                <li>Xxx , xxx, 999-999</li>
+                <li>Yyy , yyy, 999-999</li>
+            </ul>
+        </div>
+    );
+
+}
+
+export default MyProfile;
+
