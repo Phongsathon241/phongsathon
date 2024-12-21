fdbda6b88c1da96f8a12982c19cd2b0a7edb2966
Phongsathon241 <phongsathon.tha@rmutto.ac.th>
2024-12-21 12:14:25 +0700

    update
    week6

--- app/routes/GetToDoLists.tsx
+++ app/routes/GetToDoLists.tsx
@@ -1,10 +1,12 @@
 import { space } from "postcss/lib/list";
 import { toDoItem } from "./toDolists";
+import MyMenu from "./template/mymenu";
+import MyFooter from "./template/myfooter";
+
 
 function CompletedCheck ({c} : {c:boolean}){
     if(c)
         return <span>{'💋'}</span>;
-
     return <span>{'💋'}</span>;
   //  {c ? <span>&x128077</span> : <span>&x128078</span>; 
 
@@ -17,7 +19,7 @@ export default function ToDolists (){
 
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <b className="text-base">
-            <CompletedCheck c={item.compleded} />
+            <CompletedCheck c={item.completed} />
             </b>
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title + "(รหัส: " + item.id + ")"}</h5>
         <p className="font-normal text-gray-700 dark:text-gray-400">{item.created}</p>
@@ -29,8 +31,10 @@ export default function ToDolists (){
     );
     return (
         <div className="m-3">
+            <MyMenu />
             <h1 className="text-xl">สิ่งที่ต้องทำ</h1>
             {listItems}
+            <MyFooter />
         </div>
       
     );
--- app/routes/toDolists.tsx
+++ app/routes/toDolists.tsx
@@ -3,18 +3,18 @@ export const toDoItem = [
     id: 1,
     title: "กิจกรรรม xxx",
     created: "2024-12-14 09:46:00",
-    compleded: true
+    completed: true
     },
     {
         id: 2,
         title: "กิจกรรรม yyy",
         created: "2024-12-14 10:40:00",
-        compleded: false
+        completed: false
     },
     {
         id: 3,
         title: "กิจกรรรม zzz",
         created: "2024-12-14 13:00:00",
-        compleded: false
+        completed: false
      }
 ];
